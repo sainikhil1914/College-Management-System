f046bb0637462c6b17668b2dd12375be95469bb7
sainikhil1914 <117729256+sainikhil1914@users.noreply.github.com>
2025-05-06 23:01:24 +0530

    Add files via upload

---
+++ user/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from user.models import *
+# Register your models here.
+admin.site.register(LoginDetails)
+admin.site.register(Semester)
+admin.site.register(Attendance)
+admin.site.register(Queries)
+admin.site.register(TimeTable)
+admin.site.register(Assignments)
+admin.site.register(Mid1)
+admin.site.register(Mid2)
+admin.site.register(TotalAttendance)
+admin.site.register(Message)
